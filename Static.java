3db8d02867e137312fc0ed9bbf7f9f4ef67780a2
PK <priyanka.kumawat0086@gmail.com>
2018-01-17 20:23:33 +0530

    Notes on Static in java

---
+++ Static.java
@@ -0,0 +1,16 @@
+(1)Static:
+-Static is a non-access modifier.
+-It is used for memory management.
+-It is applicable for :
+	1)variable 
+	2)method
+	3)block
+	4)class
+
+(2)Static variable:
+-It is common to all instance of class
+-It is class level variable
+-Only one single copy of static variable is created and shared among all instance
+ of the class
+-Memory allocation of such variable happens at the time of class loading.
+-Static variable are initialized before the initialization of the instance variable.
